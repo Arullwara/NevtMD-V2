089a46359d7aa7c99bea5af40e7ae7fa3498d04f
Arullwara <106657360+Arullwara@users.noreply.github.com>
2022-06-05 20:09:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ second: 'numeric'
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
-}global.owner = ['6281805450249'] // Put your number here
+}global.owner = ['6285822347348'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -61,26 +61,26 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'NEVT-MD'
-global.author = `N E V T－MD
+global.packname = 'ArullBotz-MD'
+global.author = `Arull－MD
 
 
 
 
 
 ig: senpai_chan_gemoy`
-global.wm = 'N E V T－ＭＤ'
+global.wm = 'Arull－ＭＤ'
 global.watermark = wm
 global.botdate = '╭──────────═┅═─────────\n│◪ ${time}\n┊◪ ${week} ${date}\n╰──────────═┅═─────────'
 global.media = 'https://telegra.ph/file/778acaaf9c0c5ec6265a3.jpg'
-global.instagram = 'https://instagram.com/NevtGanz'
+global.instagram = 'https://instagram.com/sahrulwara_____'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 
-global.multiplier = 69 // The higher, The harder levelup
+global.multiplier = 100 // The higher, The harder levelup
 
 global.rpg = {
   emoticon(string) {
